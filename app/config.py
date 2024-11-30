837262dbf20333da3e8cbd7b9ce1549c492e63ae
grechka <gnmlach@gmail.com>
2024-12-01 02:04:32 +0300

    Перенастройка на подключение к postgresdb

--- app/config.py
+++ app/config.py
@@ -3,10 +3,22 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    BASE_DIR: str = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-    DB_URL: str = f"sqlite+aiosqlite:///{BASE_DIR}/data/db.sqlite3"
     SECRET_KEY: str
     ALGORITHM: str
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_GRECHKA_CRM: str
+    DB_DVIZH_TOUR: str
+
+    def get_grechka_crm__db_url(self):
+        return (f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@"
+                f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_GRECHKA_CRM}")
+
+    def get_dvizh_tour_db_url(self):
+        return (f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@"
+                f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_DVIZH_TOUR}")
 
     model_config = SettingsConfigDict(env_file=f"{BASE_DIR}/.env")
 
--- app/dao/database.py
+++ app/dao/database.py
@@ -4,9 +4,9 @@ from sqlalchemy import func, TIMESTAMP, Integer
 from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, declared_attr
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine, AsyncSession
 
-from app.config import database_url
+from app.config import settings
 
-engine = create_async_engine(url=database_url)
+engine = create_async_engine(url=settings.get_grechka_crm__db_url)
 async_session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 str_uniq = Annotated[str, mapped_column(unique=True, nullable=False)]
 
